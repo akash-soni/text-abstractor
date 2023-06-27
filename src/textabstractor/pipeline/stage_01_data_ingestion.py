d6ebb8da4f9c626fa841e73c3cfdebd6afd36b1b
akash-soni <akash.200287@gmail.com>
2023-06-27 11:45:39 +0530

    data ingestion completed

---
+++ src/textabstractor/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,18 @@
+from textabstractor.config.configuration import ConfigurationManager
+from textabstractor.components.data_ingestion import DataIngestion
+from textabstractor.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise e
